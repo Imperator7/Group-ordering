4e64441c1db1181a82efafe76ab2b4173deba2e4
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-12-09 03:57:18 +0700

    feat(api): add patch method for client side

--- lib/client/api.ts
+++ lib/client/api.ts
@@ -102,6 +102,36 @@ export async function Put<I, O>(
   }
 }
 
+export async function Patch<I, O>(
+  url: string,
+  input: I,
+  inputSchema: ZodType<I>,
+  outputSchema: ZodType<O>
+): Promise<O> {
+  const parsedInput = inputSchema.safeParse(input)
+
+  if (!parsedInput.success) {
+    console.error('Request validation failed:', parsedInput.error)
+    throw new ApiError('Invalid request payload')
+  }
+
+  try {
+    const res = await axios.patch(url, parsedInput.data)
+    const parsedOutput = outputSchema.safeParse(res.data.data)
+    console.log(res, parsedOutput)
+
+    if (!parsedOutput.success) {
+      console.error('Response validation failed:', parsedOutput.error)
+      throw new ApiError('Invalid response format')
+    }
+
+    return parsedOutput.data
+  } catch (err) {
+    if (err instanceof ApiError) throw err
+    return handleAxiosError(err)
+  }
+}
+
 export async function Delete(url: string): Promise<void> {
   try {
     await axios.delete(url)
